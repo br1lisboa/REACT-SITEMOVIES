88e9684447a02792ee19a94b5c8bfe49a99c7ad4
br1lisboa <brlisbo@gmail.com>
2022-07-29 15:46:10 -0300

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import swAlert from 'sweetalert2';
+
+function Login() {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    console.log(regexEmail.test(email));
+
+    if (email === '' || password === '') {
+      swAlert.fire({
+        title: 'Error!',
+        text: 'Debes rellenar los campos de login',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+    if (email !== '' && !regexEmail.test(email)) {
+      swAlert.fire({
+        title: 'Error!',
+        text: 'Debes escribir una direccion de correo electronico valida',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+    if (email !== 'challenge@alkemy.org' || password !== 'react') {
+      swAlert.fire({
+        title: 'Error!',
+        text: 'Los datos ingresados son erroneos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+
+    axios
+      .post('http://challenge-react.alkemy.org', { email, password }) // Los dos parametros que recibe axio son, la url que tiene el endpoint, y los datos que se enviaran.
+      .then(res => {
+        swAlert.fire({
+          title: 'Bienvenido',
+          text: 'Has ingresado de manera correcta',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        })
+        const tokenRecibido = res.data.token;
+        localStorage.setItem('token', tokenRecibido); // Localstorage recibe 2 argumentos, el nombre bajo el cual se guarda, y los datos que se recibe
+      })
+  }
+
+  return (
+    <>
+      <h2>Formulario de login</h2>
+      <form onSubmit={submitHandler}>
+        <label>
+          <span>Correo electronico</span>
+          <br />
+          <input type="text" name="email" />
+          <br />
+          <br />
+        </label>
+        <label>
+          <span>Contraseña</span>
+          <br />
+          <input type="password" name="password" />
+          <br />
+          <br />
+        </label>
+        <button type="submit">Ingresar</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
