017a2f922b6e43c7a80d116c5214933673ed1e8e
br1lisboa <brlisbo@gmail.com>
2022-07-30 19:07:25 -0300

    creados header y main

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+        <nav>
+            <ul>
+                <li><a href="www.twiter.com" rel='noopener-noreferrer'>Tw</a></li>
+            </ul>
+        </nav>
+        <p>Derechos Reservados</p>
+    </footer>
+  )
+}
+
+export default Footer;
