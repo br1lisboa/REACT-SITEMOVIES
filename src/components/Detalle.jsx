99810034141f7b455607c56fb4c010917012b843
br1lisboa <brlisbo@gmail.com>
2022-08-10 10:34:07 -0300

    trabajando en el componente detalle

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Container = styled.div`
-        width: 100%;
-        height: 100vh;
-    `;
 
 const Detalle = () => {
 
-
     const token = sessionStorage.getItem('token');
 
+    //Vamos a obtener el ID que viaja por URL a el componente detalle.
+    let query = new URLSearchParams(window.location.search);
+    let movieID = query.get('movieID');
+    console.log(movieID);
+
     return (
-        <Container>
+        <div className='h-screen'>
             {!token && <Navigate to='/' replace='false' />}
             <h2>Detalle</h2>
-        </Container>
+        </div>
     )
 };
 
